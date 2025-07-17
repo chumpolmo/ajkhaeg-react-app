f5baa3b695cb07f9382383135aff19f1ad67060d
Beritokai <chumpol_mo@rmutto.ac.th>
2025-07-17 12:18:57 +0700

    my first component

---
+++ app/routes/khaeg-lab02.tsx
@@ -0,0 +1,14 @@
+function MyProfile () {
+  return (
+    <div>
+        <p className="text-2xl">Khage Profile</p>
+        <img src="/images/myprofile.png" className="w-1/4" />
+        <p className="text-lg">
+          Name: Chumpol Mokarat<br />
+          Student ID: 071725681203-0<br />
+        </p>
+    </div>
+  );
+}
+
+export default MyProfile
